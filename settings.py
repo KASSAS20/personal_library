9a2912376301e2382fe1eb9ca5b9e210ec50c9b2
kassas20 <kassas20@mail.ru>
2024-02-13 21:13:01 +0400

    config

---
+++ settings.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    DB: str
+    USER: str
+    PASSWORD: str
+    HOST: str
+    PORT: str
+    SECRET_KEY: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
+
+print(settings.SECRET_KEY)
